d11c221853f6c8104e4a592a10be7c0176bf696c
Sergey Nikonov <sergey.nikonov@simbirsoft.com>
2021-05-07 16:51:05 +0300

    Initial Commit

---
+++ examples/example.php
@@ -0,0 +1,7 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use snikonov\hw3\hw3;
+
+hw3::sayHi();
---
+++ src/hw3.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace snikonov\hw3;
+
+class hw3 {
+
+    public static function sayHi() {
+        echo "Hello World" . PHP_EOL;
+    }
+
+}
